f14d18c6e8787a27274103c8f3c6f8007f134982
jsachinganesh <jsachinganesh@gmail.com>
2021-02-04 19:40:47 +0530

    commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,35 @@
 const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json','utf-8'));
 
+
+exports.checkID = (req,res,next,val)=>{
+
+    if(val*1>tours.length){
+        return res.status(404).json({
+            status:'success',
+            message:"Invalid ID 🐨"
+        })
+    }
+
+    next();
+}
+
+
+exports.checkBody = (req,res,next) => {
+    const {name,price} = req.body;
+    console.log(name,price);
+    if(!name || !price){ // 400 bad req
+        return res.status(400).json({
+            status:'success',
+            message:'A tour must have price and name'
+        });
+    }
+    next();
+}
+
 exports.getAllTours = (req,res)=>{
     res.status(200).json({
-        status:'success',
+        status:'fail',
         results:tours.length,
         data:{
             tours
@@ -31,14 +57,7 @@ exports.getTour = (req,res)=>{
 
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if(!tour){
-        return res.status(404).json({
-            status:"success",
-            message:"Invalid ID"
-        })
-    }
-
+    
     res.status(200).json({
         status:'success',
         createdAt:req.requestTime,
@@ -52,13 +71,6 @@ exports.updateTour = (req,res)=>{
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
 
-    if(!tour){
-        return res.status(404).json({
-            status:"success",
-            message:"Invalid ID"
-        })
-    }
-
     res.status(200).json({
         status:'success',
         data:{
@@ -70,13 +82,6 @@ exports.updateTour = (req,res)=>{
 exports.deleteTour = (req,res)=>{
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if(!tour){
-        return res.status(404).json({
-            status:"success",
-            message:"Invalid ID"
-        })
-    }
 
     res.status(204).json({ // 204 delete
         status:'success',
