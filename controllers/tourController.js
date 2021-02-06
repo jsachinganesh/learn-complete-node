0c4445745f0cf33e417ef9e7f789155b2b1db8e8
jsachinganesh <jsachinganesh@gmail.com>
2021-02-06 16:17:12 +0530

    commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,35 +1,31 @@
 const Tour = require('./../models/tourModal');
+const APIFeatures = require('./../utils/apiFeatures'); 
+
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+};
 
 exports.getAllTours = async (req,res)=>{
     try {
 
-        const queryObj = {...req.query}
-        const excludedFields = ['page','sort','limit','fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-
-        const query =  Tour.find();
-
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); // replace with callback
-
-        if(req.query.sort){
-            const sortBy = req.query.sort.split(',').join(' ');
-            query.sort(sortBy)
-        }else{
-            query.sort('-createdAt')
-        }
-
-        const tours = await query.find(JSON.parse(queryStr)).select("name price duration")
-
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const doc = await features.query; 
         res.status(200).json({
             status:'success',
-            results:tours.length,
+            results:doc.length,
             data:{
-                tours
+                Tours:doc
             }
         });
     } catch (error) {
-        res.status(501).json({
+        res.status(404).json({
             status:'fail',
             message: error
         })
