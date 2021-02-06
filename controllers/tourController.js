ed8665f6132f6c6d67c21da27011cf9115299c78
jsachinganesh <jsachinganesh@gmail.com>
2021-02-06 12:50:40 +0530

    commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,25 @@ const Tour = require('./../models/tourModal');
 
 exports.getAllTours = async (req,res)=>{
     try {
-        const tours = await Tour.find();
+
+        const queryObj = {...req.query}
+        const excludedFields = ['page','sort','limit','fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        const query =  Tour.find();
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); // replace with callback
+
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query.sort(sortBy)
+        }else{
+            query.sort('-createdAt')
+        }
+
+        const tours = await query.find(JSON.parse(queryStr)).select("name price duration")
+
         res.status(200).json({
             status:'success',
             results:tours.length,
